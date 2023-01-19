c2518136f0f6b1c6e84616adabb9621a8409885f
Thanh Nguyen <thanhn1756@gmail.com>
2023-01-19 07:43:05 -0800

    implemented svg highlighting for entourages

--- components/imageBox.js
+++ components/imageBox.js
@@ -7,52 +7,24 @@ import { motion } from 'framer-motion';
 import DescriptionTab from './descriptionTab';
 import Entourage from "./entourage";
 import * as d3 from "d3";
-import Entourages from "./entourages";
 export default function ImageBox({ journey }) {
   const { width, height } = useWindowSize();
-
+  const [isOpen, setIsOpen] = useState(false);
   const ref = useRef(null);
   const svgRef = useRef(null);
   useEffect(() => {
     // console.log(journey.body);
-    const svg = d3.select(svgRef.current);
-    // journey.entourages.forEach((entourage,i) => {
-    //   d3.xml(entourage.element)
-    //     .then(data => {
-    //       const g = svg.append("g")
-    //         .attr("transform",`translate(0, ${i*500})`)
-    //         .html(data.documentElement.innerHTML);
-    //
-    //       g.selectAll("*")
-    //         .on("mouseover", function () {
-    //           d3.select(this)
-    //             .style("opacity", 0.5);
-    //         })
-    //         .on("mouseout", function () {
-    //           d3.select(this)
-    //             .style("fill", "white")
-    //             .style("opacity", 1);
-    //         });
-    //     });
-    // });
+
 
   }, [journey]);
   const entourages = journey.entourages.map((entourage) => (
     <>
       <TextBox
+        entourage={entourage}
         key={entourage.id}
-        posX={entourage.posX}
-        posY={entourage.posY}
-        text={entourage.body}
+        svgRef={svgRef}
+        isOpen={isOpen}
       />
-      {/* <motion.div whileHover={{ scale: 1.2 }}> */}
-      {/*<object*/}
-      {/*  type="image/svg+xml"*/}
-      {/*  data={entourage.element}*/}
-      {/*  style={{ position: 'absolute', height: height }}*/}
-      {/*></object>*/}
-      <Entourage entourage={entourage} width={width} height={height}/>
-      {/* </motion.div> */}
 
     </>
   ));
@@ -69,13 +41,9 @@ export default function ImageBox({ journey }) {
           data={journey.imageUrl}
           style={{ position: 'absolute', height: height }}
           // className={styles.image}
+          ref={svgRef}
         ></object>
         {entourages}
-        {/*<div>*/}
-        {/*  <svg ref={svgRef} />*/}
-        {/*</div>*/}
-        {/*<svg id="svg"> </svg>*/}
-        {/*<Entourages svgFiles={journey.entourages}/>*/}
       </div>
     </>
   );
