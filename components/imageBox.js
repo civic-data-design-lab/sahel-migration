7a9317c4045f7236d2012b03d0c033bface2f352
Thanh Nguyen <thanhn1756@gmail.com>
2023-01-24 17:10:24 -0800

    scroll triggers animation for journey implemented

--- components/imageBox.js
+++ components/imageBox.js
@@ -3,13 +3,19 @@ import styles from '../styles/ImageBox.module.css';
 import useWindowSize from '../hooks/useWindowSize';
 import Title from './title';
 import { useState, useRef, useEffect } from 'react';
-import { motion } from 'framer-motion';
 import DescriptionTab from './descriptionTab';
 import * as d3 from "d3";
+import {
+  motion,
+  useTransform,
+} from "framer-motion";
+
+function useParallax(value, distance) {
+  return useTransform(value, [0, 1], [0, distance]);
+}
+
 export default function ImageBox({ journey }) {
   const { width, height } = useWindowSize();
-  // const { width, height, refWin } = useResizeDetector();
-  const [isOpen, setIsOpen] = useState(false);
   const ref = useRef(null);
   const svgRef = useRef(null);
   useEffect(() => {
@@ -18,24 +24,22 @@ export default function ImageBox({ journey }) {
 
   }, [journey]);
   const entourages = journey.entourages.map((entourage) => (
-    <>
       <Card
         entourage={entourage}
         key={entourage.id}
         svgRef={svgRef}
-        isOpen={isOpen}
+        scrollRef={ref}
         width={width}
         height={height}
       />
 
-    </>
   ));
   const scrollToCoordinate = (posX, posY) => {
     ref.current.scrollLeft = posX;
   };
   return (
     <>
-      <div className="box" ref={ref} style={{position: 'relative'}}>
+      <motion.div className="box" ref={ref} styles={{position: 'relative'}}>
         <Title />
         <DescriptionTab title={journey.title} body={journey.body} />
         <object
@@ -47,7 +51,7 @@ export default function ImageBox({ journey }) {
         >
         </object>
         {entourages}
-      </div>
+      </motion.div>
     </>
   );
 }
