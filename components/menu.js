af9751b72ca9f84490ed7aad2057a65361dc49e3
Thanh Nguyen <thanhn1756@gmail.com>
2023-01-25 22:54:09 -0500

    changed menu bar navigation styling;

--- components/menu.js
+++ components/menu.js
@@ -3,7 +3,12 @@ import { useSpring, animated, useTransition, config } from 'react-spring';
 import { useRouter } from 'next/router';
 import styles from '../styles/Menu.module.css';
 import Link from "next/link";
+import Card from "./card";
+import useSWR from "swr";
+import {fetcher} from "../hooks/useFetch";
 export default function Menu() {
+  const _id = 'all';
+  const {data: journeys, error} = useSWR(['/api/journeysdata',_id], fetcher);
   const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
   const router = useRouter();
   const [menuOpen, setMenuOpen] = useState(false);
@@ -20,58 +25,70 @@ export default function Menu() {
     };
   };
 
-  const fullscreenMenu = useSpring({
-    opacity: menuOpen ? 1 : 0,
-    y: menuOpen ? 0 : 1000,
-    config: { tension: 170, friction: 26, precision: 0.01, clamp: true },
-    position: 'fixed',
-    bottom: 0,
-    right: 0,
-    left: 0,
-    top: 0,
-  });
 
+  const fullscreenMenu = useSpring({
+    // opacity: menuOpen ? 1 : 0,
+    // config: { tension: 170, friction: 26, precision: 0.01, clamp: true },
+    // position: 'fixed',
+    // bottom: 0,
+    from: { right: "-100%" },
+    right: menuOpen ? "0" : "-100%",
+    // left: 0,
+    // top: 0,
+  });
+  if (error) return <div>Journey not found</div>;
+  if (!journeys) return<></>;
   return (
-    <nav className={styles.navBar}>
+    <nav>
       <span className={styles.menuContainer}>
         {!menuOpen ? (
           <button onClick={handleToggle} className={styles.menuButton}>
             <span
               class="material-symbols-outlined"
-              style={{ color: 'var(--brown)' }}
+              style={{ color: 'black' }}
             >
               menu
             </span>
           </button>
         ) : (
           <button onClick={handleToggle} className={styles.menuButton}>
-            <span class="material-symbols-outlined" style={{ color: 'white' }}>
+            <span class="material-symbols-outlined" style={{ color: 'black' }}>
               close
             </span>
           </button>
         )}
       </span>
       <div>
-        <animated.div style={fullscreenMenu}>
-          <ul className={styles.menuSelection}>
+        <animated.div  style={{
+                              opacity: menuOpen ? 1 : 0,
+                              zIndex: menuOpen? 9000:0,
+                              transitionTimingFunction: menuOpen? 'ease-in': 'ease-out',
+        }} className={styles.screenCover}/>
+        <animated.div style={fullscreenMenu} className={styles.navBar}>
+          <ul>
             <li>
-              <Link onClick={handleRouting('/')} href="/">
-                HOME
+              <Link className={styles.route} onClick={handleRouting('/')} href="/">
+                Map
               </Link>
             </li>
             <li>
-              <Link onClick={handleRouting('/about')} href="/about">
-                ABOUT
+              Journey
+              <ul>
+                {journeys.map((entourage) => (
+                  <li key={entourage.id}>
+                    <Link className={styles.route} key={entourage.id} href={'/journeys/'+entourage.id}>{entourage.title}</Link>
+                  </li>
+                ))}
+              </ul>
+            </li>
+            <li>
+              <Link className={styles.route} onClick={handleRouting('/about')} href="/about">
+                Current Conditions in Libya
               </Link>
             </li>
             <li>
-              <Link onClick={handleRouting('/credits')} href="/credits">
-                CREDITS
-              </Link>
-            </li>
-            <li>
-              <Link onClick={handleRouting('/data-source')} href="/data-source">
-                DATA SOURCE
+              <Link className={styles.route} onClick={handleRouting('/about')} href="/data-source">
+                About
               </Link>
             </li>
           </ul>
