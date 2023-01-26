60aac48a1721f00b4083cbdbd24785c96020c0f7
Thanh Nguyen <thanhn1756@gmail.com>
2023-01-26 10:30:27 -0500

    revert windowSize implementation

--- hooks/useWindowSize.js
+++ hooks/useWindowSize.js
@@ -3,32 +3,24 @@ import { useState, useEffect } from "react";
 
 
 export default function useWindowSize() {
-    // Initialize state with undefined width/height so server and client renders match
-    // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+    const isSSR = typeof window == "undefined";
     const [windowSize, setWindowSize] = useState({
-        width: undefined,
-        height: undefined,
+        width: isSSR ? 1200 : window.innerWidth,
+        height: isSSR ? 800 : window.innerHeight,
     });
 
+
+
     useEffect(() => {
-        // only execute all the code below in client side
-        // Handler to call on window resize
-        function handleResize() {
-            // Set window width/height to state
-            setWindowSize({
-                width: window.innerWidth,
-                height: window.innerHeight,
-            });
+        function changeWindowSize() {
+            setWindowSize({ width: window.innerWidth, height: window.innerHeight });
         }
+        window.addEventListener("resize", changeWindowSize);
 
-        // Add event listener
-        window.addEventListener("resize", handleResize);
+        return () => {
+            window.removeEventListener("resize", changeWindowSize);
+        };
+    }, []);
 
-        // Call handler right away so state gets updated with initial window size
-        handleResize();
-
-        // Remove event listener on cleanup
-        return () => window.removeEventListener("resize", handleResize);
-    }, []); // Empty array ensures that effect is only run on mount
     return windowSize;
 }
