0cb3732711ae3766c978fcb917f213c5d4c94154
Sebastian Ives <tonykeith99@yahoo.com>
2023-01-31 17:00:00 -0500

    removed unused box component

--- components/contentBox.js
+++ components/contentBox.js
@@ -1,17 +1,16 @@
-import { createContext, useRef, useEffect, useContext, useState } from 'react'
-import { motion, useScroll, useInView } from 'framer-motion'
+import { useRef, useEffect, useContext } from 'react'
+import { useInView } from 'framer-motion'
 import styles from './../styles/ContentBox.module.css'
 import { ViewContext } from '../pages/maps/map'
 
 
-function Paragraph({ children, scrollRef, data }) {
+function Paragraph({ children, data }) {
     const ref = useRef(null)
     const isInView = useInView(ref, {
         amount: 1
     })
     const { currentView, setCurrentView } = useContext(ViewContext)
     useEffect(() => {
-        console.log("Element is in view: ", isInView)
         if (isInView) {
             setCurrentView(data.id)
         }
@@ -25,16 +24,7 @@ function Paragraph({ children, scrollRef, data }) {
     )
 }
 
-function Box({ isVisible }) {
-    return (
-        <div className={styles.box}>
-            <h3></h3>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae doloribus, quibusdam consequatur libero odit aspernatur nihil. Quasi repellat enim accusamus iure nemo aliquid! Libero beatae nam expedita. Asperiores esse error temporibus amet maiores possimus explicabo ut sint iusto accusantium quo at eum maxime beatae aliquam aspernatur, laborum, eveniet quam alias!</p>
-        </div>
-    );
-}
-
-export default function ContentBox({ body, scrollRef, dataItems }) {
+export default function ContentBox({ dataItems }) {
     const contentRef = useRef(null)
     return (
         <div ref={contentRef} className={styles.container}>
@@ -43,7 +33,6 @@ export default function ContentBox({ body, scrollRef, dataItems }) {
                     return (
                         <div className={styles.paragraphContainer}  >
                             <Paragraph
-                                scrollRef={contentRef}
                                 data={data}>
                             </Paragraph>
                         </div>
