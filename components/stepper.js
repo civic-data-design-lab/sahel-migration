2ef7c7e46f311fc7e218ba408e5bf66edd12da7d
Thanh Nguyen <thanhn1756@gmail.com>
2023-02-09 02:50:24 -0500

    added stepper for navigation

--- components/stepper.js
+++ components/stepper.js
@@ -1,14 +1,34 @@
 import styles from '../styles/Stepper.module.css';
-
-export default function Stepper({ totalSteps, stepNumber }) {
-  const steps = [...Array(totalSteps + 1).keys()];
+import { motion } from "framer-motion"
+export default function Stepper({ totalSteps, stepNumber, journeys, reversed, isActive}) {
+  const steps = [...Array(totalSteps).keys()];
+  const variants = {
+    open: {
+      opacity: 1,
+      // transition: { staggerChildren: 1, delayChildren: 0.2 }
+    },
+    closed: {
+      opacity: 0,
+      // transition: { staggerChildren: 0.05, staggerDirection: -1 }
+    }
+  };
   function renderSteps(currentStep, stepNumber) {
     const isFilled = currentStep < stepNumber ? styles.filled : '';
-    return <div className={`${styles.step} ${isFilled}`}></div>;
+    const isCurrent = currentStep === stepNumber? styles.current : '';
+    return (
+      <div className={styles.stepContainer} style={{flexDirection: reversed? "row-reverse":""}}>
+        <div className={`${styles.step} ${isFilled} ${isCurrent}`}/>
+        <small  className={styles.textContainer}>{journeys.find(item => item.id ===currentStep+1).title}</small>
+      </div>
+      );
   }
   return (
-    <div className={styles.stepper}>
-      {steps.map((currentStep) => renderSteps(currentStep, stepNumber))}
-    </div>
+    <motion.div className={styles.stepperContainer} animate={isActive ? "open" : "closed"} variants={variants}>
+      <div className={styles.stepperLine} style={{transform: reversed? "translate(8rem,-65%)": ''}}/>
+      <motion.div className={styles.stepper} animate={isActive ? "open" : "closed"} variants={variants}>
+        {steps.map((currentStep) => renderSteps(currentStep, stepNumber))}
+      </motion.div>
+    </motion.div>
+
   );
 }
