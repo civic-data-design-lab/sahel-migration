7260efce2706e25980911009037d55a3ec09a8bd
Sebastian Ives <tonykeith99@yahoo.com>
2023-02-18 22:06:42 -0500

    added nav items to risk data

--- components/riskItems.js
+++ components/riskItems.js
@@ -1,9 +1,21 @@
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
 import styles from "../styles/Card.module.css";
 
 
-export default  function RiskItems({isOpen}) {
+export default function RiskItems({ isOpen }) {
+  const transectRisks = [
+    "Migration Risks",
+    "Proximity to Urban Areas",
+    "Reported Violence",
+    "Conlfict Areas",
+    "Food Prices",
+    "Photos"
+  ]
+
   return (
-  <>
-  </>)
+    <>
+      {transectRisks.map((risk) => {
+        <div key={risk}>risk</div>
+      })}
+    </>)
 }
--- pages/maps/map.js
+++ pages/maps/map.js
@@ -11,19 +11,22 @@ import { animated, useSpring } from "react-spring";
 import Menu from "../../components/menu";
 
 
-const fetcher = (url) => fetch(url).then((res) => res.json());
+
+const mapFetcher = (url) => fetch(url).then((res) => res.json());
 export const ViewContext = createContext({
     currentView: 'overallRoutes',
     setCurrentView: (() => { })
 })
 
 export default function MainMap() {
+
+
     const { width } = useWindowSize()
     const sideBarRef = useRef(null)
     const [currentView, setCurrentView] = useState('overallRoutes')
     const viewValue = { currentView, setCurrentView }
+    const { data: riskItems, error: risksError } = useSWR('/api/map/risksdata', mapFetcher)
 
-    const { data: riskItems, error: risksError } = useSWR('/api/map/risksdata', fetcher)
 
     const isActive = currentView === 'selectRoute' ? true : false
 
@@ -31,13 +34,15 @@ export default function MainMap() {
         marginBottom: (isActive && width < 480) ? '0rem' : '0'
     });
 
-    if (risksError) return <div>Journey not found</div>;
+    if (risksError) return <div>Map not found</div>;
     if (!riskItems) return <div>loading...</div>;
 
     return (
 
         <ViewContext.Provider value={viewValue}>
             <Title />
+            {/* <Menu journeys={journeys} /> */}
+
             <div className={styles.boxContainer}>
                 <div className={styles.contentBox} ref={sideBarRef}>
                     <ContentBox scrollRef={sideBarRef} dataItems={riskItems.risks} />
