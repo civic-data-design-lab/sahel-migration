f65eb17c69746092ef8b857eca8f30339f345ad7
Thanh Nguyen <thanhn1756@gmail.com>
2023-04-09 21:57:08 -0400

    updated to have accordion styling

--- components/transectPlots.js
+++ components/transectPlots.js
@@ -1,39 +1,51 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useMemo, useRef} from 'react';
 import useSWR from "swr";
 import {fetcher} from "../hooks/useFetch";
 import Transect from "./transect";
 import {motion, AnimatePresence} from "framer-motion";
 import DataTabToggle from "./dataTabToggle";
+import Accordion from "./accordion";
 
 export default function TransectPlots ({isOpen, toggleOpen}) {
-
-  useEffect( () => {
-  })
-
-  const transectTypes = ["4mi", "ACLED","food security", "smuggler", "remoteness", "heat"]
-
-  const TransectLayers = transectTypes.map((t,i) => {
-    return <Transect risk={t} key={i}/>
-
-  })
-
+  const contentRef = useRef(null);
+  useEffect(() => {
+    if (!isOpen) {
+      contentRef.current.scrollTo(0, 0);
+    }
+  }, [isOpen]);
+  const containerStyles = {
+    overflowY: isOpen ? 'auto' : 'hidden',
+    overflowX:"scroll",
+    scrollbarWidth:"none"
+    /* Other styles */
+  };
+  const TransectLayers = useMemo(() =>  {
+    const transectTypes = ["4mi", "ACLED","food security", "smuggler", "remoteness", "heat"]
+    return transectTypes.map((t,i) => {
+    return (
+      <motion.div
+        className="accordion-content"
+        animate={{ height: isOpen ? "auto" : 0 }}
+        transition={{ duration: 0.1 }}
+        key={i}
+      >
+        <Transect risk={t} key={i}/>
+      </motion.div>
+    )
+  }) },[isOpen])
 
 
 
 
   return (
-    <div style={{overflowX:"scroll", scrollbarWidth:"none" }}>
-      <div style={{display:"flex", flexDirection:"row", justifyContent:"space-between"}}>
+    <div style={containerStyles} ref={contentRef}>
+      <div style={{display:"flex", flexDirection:"row", justifyContent:"space-between"}} >
         <DataTabToggle isOpen={isOpen} toggleOpen={toggleOpen} />
       </div>
-      <AnimatePresence>
-      {
-        isOpen
-          ? TransectLayers
-          : <Transect risk={"all"}/>
-      }
-      </AnimatePresence>
-
+      <motion.div>
+        <Transect risk={"all"}/>
+        {TransectLayers}
+      </motion.div>
     </div>
   );
 }
