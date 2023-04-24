5897cd72702c67af938269307bc17b1dac739f36
ashsicle <ashleylouie27@gmail.com>
2023-04-24 18:51:05 -0400

    title logo svg for next.js

--- components/title.js
+++ components/title.js
@@ -1,14 +1,22 @@
 import styles from '../styles/Title.module.css';
 import useWindowSize from '../hooks/useWindowSize';
+import Link from "next/link";
 
 export default function Title() {
   const { width } = useWindowSize()
 
   return (
     <div className={styles.main}>
-        <a href="/">
-            <img className={styles.title} src="../images/logo_Migrants on the Move.svg" alt="Migrants on the Move: Risks of West African Migration logo" />
-        </a>
+        <Link href="/">
+            {/* <Image className={styles.title} src="../images/logo_Migrants on the Move.svg" alt="Migrants on the Move: Risks of West African Migration logo" /> */}
+            <object
+                type="image/svg+xml"
+                data="../images/logo_Migrants on the Move.svg"
+                className={styles.title}
+                alt="Migrants on the Move: Risks of West African Migration logo"
+            >
+            </object>
+        </Link>
     </div>
   );
 }
