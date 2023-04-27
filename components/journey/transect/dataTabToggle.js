b4a63ed559e717da7252e8647094d54a92f1cd4d
jari <90732276+jprachasartta@users.noreply.github.com>
2023-04-27 11:15:37 -0400

    Update dataTabToggle.js

--- components/journey/transect/dataTabToggle.js
+++ components/journey/transect/dataTabToggle.js
@@ -6,9 +6,9 @@ export default function DataTabToggle({ isOpen, toggleOpen }) {
       <button className={styles.toggleButton} onClick={toggleOpen}>
         <h3 className="font-sans">Explore the Risks</h3>
         {isOpen ? (
-          <span className="material-symbols-outlined">expand_more</span>
+          <span className="material-symbols-outlined" style={{ color: '#463c35', fontSize: '2rem' }}>expand_more</span>
         ) : (
-          <span className="material-symbols-outlined">expand_less</span>
+          <span className="material-symbols-outlined" style={{ color: '#463c35', fontSize: '2rem' }}>expand_less</span>
         )}
       </button>
     </>
